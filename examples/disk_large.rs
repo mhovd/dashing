46306303da398570dfddc0286fa58d215757a08a
Markus <66058642+mhovd@users.noreply.github.com>
2024-08-28 10:46:29 +0200

    Large example

---
+++ examples/disk_large.rs
@@ -0,0 +1,24 @@
+use dashing::unbounded::Unbounded;
+use dashing::Cache;
+
+fn main() -> Result<(), anyhow::Error> {
+    type K = i32;
+    type V = Vec<i32>;
+
+    let cache: Unbounded<K, V> = Unbounded::new();
+
+    for i in 0..1E6 as i32 {
+        // Insert random f64 values into the cache
+        cache.insert(i, (0..1E3 as i32).collect());
+    }
+
+    println!("Writing cache to file...");
+    cache.write_to_file("dashing.cache")?;
+
+    let cache2: Unbounded<K, V> = Unbounded::new();
+
+    println!("Reading cache from file...");
+    cache2.read_from_file("dashing.cache")?;
+    println!("Cache contains {} items", cache2.len());
+    Ok(())
+}
